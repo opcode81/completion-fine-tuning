afa4dc49ea4fd02b007a805a2145f898843fa54a
Dominik Jain <d.jain@appliedai.de>
2023-06-01 16:11:49 +0200

    Added training script for Ruby

---
+++ train_ruby.sh
@@ -0,0 +1,20 @@
+DATE=$(date +'%Y%m%d-%H%M')
+python train.py \
+        --model_path="bigcode/santacoder" \
+        --dataset_name="bigcode/the-stack-dedup" \
+        --subset="data/ruby" \
+        --data_column "content" \
+        --split="train" \
+        --seq_length 2048 \
+        --max_steps 30000 \
+        --batch_size 2 \
+        --gradient_accumulation_steps 8 \
+        --learning_rate 5e-5 \
+        --num_warmup_steps 500 \
+        --eval_freq 500 \
+        --save_freq 50 \
+        --log_freq 1 \
+        --num_workers="$(nproc)" \
+        --no_fp16 \
+        --fim_rate 0.5
+#  2>&1 | tee train_ruby_$DATE.log
