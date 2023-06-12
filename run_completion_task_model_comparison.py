60c54abc709ad40cd63d07ecdd8d8c9527806d28
Dominik Jain <d.jain@appliedai.de>
2023-06-12 18:07:19 +0200

    Introduced ModelFactory which now handles model/tokenizer creation

--- run_completion_task_model_comparison.py
+++ run_completion_task_model_comparison.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 from completionft.completion_task import CompletionTaskModelComparison
+from completionft.model import SantaCoderModelFactory
 
 log = logging.getLogger(__name__)
 
@@ -10,11 +11,12 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)-5s %(asctime)-15s %(name)s:%(funcName)s - %(message)s', stream=sys.stdout,
         level=logging.INFO)
     log.info("Starting")
+    model_factory = SantaCoderModelFactory()
     ruby_models = [
         "checkpoints/ruby/checkpoint-6000",
         "checkpoints/ruby/checkpoint-500",
         "bigcode/santacoder"
     ]
-    CompletionTaskModelComparison("ruby", models=ruby_models).run()
-    #CompletionTaskModelComparison("c-sharp", models=["bigcode/santacoder"]).run()
+    CompletionTaskModelComparison("ruby", model_factory, model_paths=ruby_models).run()
+    #CompletionTaskModelComparison("c-sharp", model_factory, models=["bigcode/santacoder"]).run()
     log.info("Done")
--- run_perplexity_evaluation.py
+++ run_perplexity_evaluation.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 from completionft.eval import ModelPerplexityEvaluation
+from completionft.model import SantaCoderModelFactory
 
 log = logging.getLogger(__name__)
 
@@ -9,6 +10,7 @@ log = logging.getLogger(__name__)
 if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)-5s %(asctime)-15s %(name)s:%(funcName)s - %(message)s', stream=sys.stdout,
         level=logging.INFO)
+    model_factory = SantaCoderModelFactory()
     ruby_models = [
         "checkpoints/ruby-lora16-fp32/checkpoint-3500",
         "checkpoints/ruby-lora64-fp32/checkpoint-3000",
@@ -18,4 +20,4 @@ if __name__ == '__main__':
         "checkpoints/ruby/checkpoint-2000",
         "bigcode/santacoder"
     ]
-    ModelPerplexityEvaluation("ruby", ruby_models).run()
+    ModelPerplexityEvaluation("ruby", model_factory, ruby_models).run()
