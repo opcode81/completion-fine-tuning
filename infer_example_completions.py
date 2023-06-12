abdb44a236508f81642f80265ec3b07a22f220fd
Dominik Jain <d.jain@appliedai.de>
2023-06-12 16:32:40 +0200

    Factored out module model

--- infer_example_completions.py
+++ infer_example_completions.py
@@ -6,8 +6,9 @@ from pathlib import Path
 from typing import Dict, List
 
 import torch
-from peft import PeftModel
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline, AutoTokenizer
+
+from completionft.model import get_model, model_id_to_fn
 
 log = logging.getLogger(__name__)
 
@@ -73,23 +74,6 @@ def read_completion_tasks(lang_id) -> Dict[str, CompletionTask]:
             code = f.read()
             tasks[fn] = CompletionTask.from_code_with_todo_tag(code, lang_id)
     return tasks
-
-
-def model_id_to_fn(model_id: str):
-    return model_id.replace("/", "--")
-
-
-def model_id_from_fn(model_fn: str):
-    return model_fn.replace("--", "/")
-
-
-def get_model(model_path: str, base_model_id: str):
-    if os.path.isdir(model_path) and os.path.exists(os.path.join(model_path, "adapter_config.json")):
-        base_model = AutoModelForCausalLM.from_pretrained(base_model_id, trust_remote_code=True)
-        log.info(f"Loading PEFT model from {model_path}")
-        return PeftModel.from_pretrained(base_model, model_path)
-    else:
-        return AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
 
 
 def run(models: List[str], lang_id: str, device="cuda:0", base_model_id="bigcode/santacoder", save_results=True):
